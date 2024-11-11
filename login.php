83c6e8d8873a2dcf981a1f7efbcb1f82478f037e
Nayab Fatima Gohar <fatimagohar100@gmail.com>
2024-11-12 02:46:10 +0500

    frontend work in prog...

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A simple Hello World HTML document.">
+    <link rel="stylesheet" href="css/login.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="loginform">
+        hello
+</body>
+</html>
